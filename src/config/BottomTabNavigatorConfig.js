fb27d1a3872846063d28e6b50b1599f92a41440a
Eric Wen <wei-yuan.wen@appfolio.com>
2018-12-07 18:02:59 -0800

    Create BottomTabNavigatorConfig

---
+++ src/config/BottomTabNavigatorConfig.js
@@ -0,0 +1,10 @@
+import Home from '../containers/Home';
+
+const BottomTabNavigatorConfig = {
+  initialRouteName: Home,
+  tabBarOptions: {
+    activeTintColor: '#e91e63',
+  },
+};
+
+export default BottomTabNavigatorConfig;
